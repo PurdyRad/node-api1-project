426c87029bb0bc845a49323433b9a44fe8d2e728
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-05 15:39:28 -0400

    finished GETS and POST

--- api/server.js
+++ api/server.js
@@ -1,3 +1,60 @@
 // BUILD YOUR SERVER HERE
+//imports
+const express = require('express');
+const User = require('./users/model.js');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+//instance of express app
+const server = express();
+
+//global middleware
+server.use(express.json())
+
+// [GET] /api/users	
+server.get('/api/users', (req,res) =>{
+    User.find()
+    .then(users => {
+        console.log('users:', users)
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        console.error('Gerr:', err)
+        res.status(500).json({message: err.message})
+    })
+})
+
+
+// [GET] /api/users/:id	
+server.get('/api/users/:id', (req,res) =>{
+    const {id} = req.params
+    User.findById(id)
+    .then(user => {
+        console.log('user:', user)
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        console.error('GIerr:', err)
+        res.status(500).json({message: err.message})
+    })
+})
+
+// [POST] /api/users	
+server.post('/api/users', (req,res) => {
+    const newUser = req.body
+    User.insert(newUser)
+    .then(user => {
+        console.log('postUser:', user)
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        console.error('Posterr:', err)
+        res.status(500).json({message: err.message})
+    })
+})
+// [PUT] /api/users/:id	
+// [DELETE] /api/users/:id	
+
+
+
+//exposing server to other modules
+module.exports = server // EXPORT YOUR SERVER instead of {}
+ 
